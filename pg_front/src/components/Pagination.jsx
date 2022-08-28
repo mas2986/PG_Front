3e9c3733bc892f704710d5d1dd6ea02fab2c199e
Federico Iglesia <federico.iglesia@outlook.com>
2022-08-28 15:10:04 -0300

    pagination and deletion of hc prods

--- pg_front/src/components/Pagination.jsx
+++ pg_front/src/components/Pagination.jsx
@@ -1,49 +1,40 @@
-import React, { useEffect, useState } from 'react';
-import { getPaginatedProduct } from '../redux/action';
-import { Box, Pagination as Pag} from '@mui/material';
-import { useDispatch, useSelector } from 'react-redux';
-import { products as pr } from '../asset/products';
+import React, { useEffect, useState } from "react";
+import { getPaginatedProduct } from "../redux/action";
+import { Box, Pagination as Pag } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+// import { products as pr } from '../asset/products';
 
 const pageSize = 6;
 
-export default function Pagination(){
+export default function Pagination() {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.products);
 
-    const dispatch = useDispatch();
-    const products = useSelector(state=>state.products)
+  const [pagination, setPagination] = useState({
+    count: 0,
+    from: 0,
+    to: pageSize,
+  });
 
-    const [pagination, setPagination] = useState({
-        count: 0,
-        from: 0,
-        to: pageSize
-    })
+  useEffect(() => {
+    dispatch(getPaginatedProduct({ from: pagination.from, to: pagination.to }));
+    console.log(products.length);
+    setPagination({ ...pagination, count: products.length });
+  }, [pagination.from, pagination.to]);
 
-    useEffect(()=>{
-        
-        dispatch(getPaginatedProduct({from: pagination.from, to: pagination.to}))
-        console.log(products.length)
-        setPagination({...pagination, count: pr.length})
-        
-    },[pagination.from, pagination.to])
+  const handlePageChange = (event, page) => {
+    const from = (page - 1) * pageSize;
+    const to = (page - 1) * pageSize + pageSize;
 
-    
-    const handlePageChange = (event, page) => {
+    setPagination({ ...pagination, from: from, to: to });
+  };
 
-        const from = (page - 1) * pageSize;
-        const to = (page - 1) * pageSize + pageSize;
-
-        setPagination({...pagination, from: from, to: to})
-
-    }
-    
-
-    return (
-        <Box justifyContent={"center"} alignItems={"center"} display={"flex"}
-        >
-        <Pag
-            count={Math.ceil(pagination.count/pageSize)}
-            onChange={handlePageChange}
-        />
-        </Box>
-    )
-
+  return (
+    <Box justifyContent={"center"} alignItems={"center"} display={"flex"}>
+      <Pag
+        count={Math.ceil(pagination.count / pageSize)}
+        onChange={handlePageChange}
+      />
+    </Box>
+  );
 }
