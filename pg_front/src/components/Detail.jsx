cc82bc83248c70a35fa0559a0596c978516e4d4a
chicha2021 <91971720+chicha2021@users.noreply.github.com>
2022-08-27 22:58:33 -0300

    COMPONENTE DETAIL

--- pg_front/src/components/Detail.jsx
+++ pg_front/src/components/Detail.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import {useParams} from 'react-router-dom';
+import { detailProduct } from '../redux/action';
+import Button from '@mui/material/Button';
 
-export default function Detail(){
+export default function Detail({match}){
     const {id} = useParams()
+    const dispatch = useDispatch()
+
+    useEffect( ()=> {
+        dispatch(detailProduct(id))
+    }, [dispatch, id])
+
+    const detail = useSelector( (state) => state.detail);
+    console.log(detail)
 
     return(
-        <>
-            <h3>Soy el detalle número {id}</h3>
-        </>
+        <div>
+            {console.log(detail)} 
+                { detail?
+                 <div>
+                     <h1>{detail.title && detail.title}</h1>
+                     <img src={detail.img? detail.img : detail.image} alt="Image not found" />
+                     <p>Brand: {detail.brand && detail.brand}</p>
+                     <p>Price: {detail.price && detail.price}</p>
+                     <p>Sport: {detail.sport && detail.sport}</p>
+                     <p>Gender: {detail.genre && detail.genre}</p>
+                     <p>{detail.description && detail.description}</p>              
+                  </div> : 
+                    <>Loading...</> 
+                }
+
+                <Button href={`http://localhost:3000/home`} variant="contained" size="small" >VOLVER</Button>
+        </div>
     )
 }
