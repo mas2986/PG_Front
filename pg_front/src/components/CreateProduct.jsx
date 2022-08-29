7ece82abdb51cdc724d77a24b580091b0662d347
Federico Iglesia <federico.iglesia@outlook.com>
2022-08-29 19:13:30 -0300

    final fixes

--- pg_front/src/components/CreateProduct.jsx
+++ pg_front/src/components/CreateProduct.jsx
@@ -1,21 +1,23 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 export default function CardProduct(props) {
   return (
-    <Card sx={{ 
-      margin:0.5,
-      width: 320,
-      height: 300,
-      background: '#FFFFFF',
-      // boxShadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px rgba(0, 0, 0, 0.14), 0px 1px 3px rgba(0, 0, 0, 0.12);
-      boxShadow:2,
-    }}>
+    <Card
+      sx={{
+        margin: 0.5,
+        width: 320,
+        height: 300,
+        background: "#FFFFFF",
+        // boxShadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px rgba(0, 0, 0, 0.14), 0px 1px 3px rgba(0, 0, 0, 0.12);
+        boxShadow: 2,
+      }}
+    >
       <CardMedia
         component="img"
         height="140"
@@ -31,8 +33,16 @@ export default function CardProduct(props) {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">BUY</Button>
-        <Button size="small">ADD TO CART</Button>
+        <Button href={"/entrega"} size="small">
+          BUY
+        </Button>
+        <Button href={"/entrega"} size="small">
+          ADD TO CART
+        </Button>
+        <Button href={`/detail/${props.id}`} size="small">
+          DETAIL
+        </Button>
+        {/* target="_blank" */}
       </CardActions>
     </Card>
   );
