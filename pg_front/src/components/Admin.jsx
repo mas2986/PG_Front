64bde43e6e06ecdd4f511d8a344377d54180a004
Marcos Saac <ing.msaac@gmail.com>
2022-09-03 17:19:13 -0300

    cambiando rutas

--- pg_front/src/components/Admin.jsx
+++ pg_front/src/components/Admin.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { NavLink , useHistory} from 'react-router-dom';
 import './Admin.css';
 
 
 function Admin() {
+    const history = useHistory()
+
+    useEffect(()=>{
+        const tokenJSON = JSON.parse(localStorage.getItem("userDetails"));
+        if (tokenJSON) {
+          const { token } = tokenJSON;
+          const { rol } = tokenJSON.data.user;
+          if(token&&rol==="user") return history.push("/login")
+        }
+        if(!tokenJSON) return history.push("/login")
+      },[])
+
     return (
         <div className='admin_container'>
             <div className='admin_item'>
-                <NavLink to='/admin/product' className="NavLink" >Agregar producto</NavLink>
+                <NavLink to='/admin/create' className="NavLink" >Agregar producto</NavLink>
             </div>
             <div className='admin_item'>
                 <NavLink to='/admin/category' className="NavLink">Agregar categoria</NavLink>
