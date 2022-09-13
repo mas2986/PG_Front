5a866d88fb3ae8543b1931517a6c5b26e32d2849
Federico Iglesia <federico.iglesia@outlook.com>
2022-09-13 20:50:58 -0300

    advancing components

---
+++ pg_front/src/components/Checkout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Checkout() {
+  return (
+    <div>
+      <h2>Please review your order and shipping information</h2>
+      <div></div>
+    </div>
+  );
+}
+
+export default Checkout;
