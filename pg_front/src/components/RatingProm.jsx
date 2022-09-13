955b030c4cbdea736cf4f50a9fff171c6ee7459b
Saymonx15 <lucianosimon2@hotmail.com>
2022-09-13 00:59:05 -0300

    Modificacion de Rating Promedio, solo queda pendiente modificar el userId por el user

---
+++ pg_front/src/components/RatingProm.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import Rating from '@mui/material/Rating';
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Popover from '@mui/material/Popover';
+import Button from '@mui/material/Button';
+import {useState} from 'react'
+
+
+
+
+
+
+export default function RatingProm(props) {
+
+
+
+  const [anchor, setAnchor] = useState(null)
+
+  const openPopover = (e) => {
+   setAnchor(e.currentTarget)
+  }
+
+
+
+
+   let id = props.id
+  const review=props.reviews
+  let quantity=0;
+  let array = [];
+  let comments = ""
+  let user = ""
+  let render =[]
+  for (let i = 0; i < review.length; i++) {
+      if(review[i].productId === Number(id)) {
+          array.push(review[i])
+          quantity += 1;
+          comments = (review[i].comment + " ")
+          user = (review[i].userId + " ")
+         render += [user +":"+ comments +"," ] 
+      } else{
+          array
+      }
+      
+  }
+  console.log(render)
+  
+  function promedio(array){
+      let suma=0;
+      let cont=0;
+      if(!array.length) return 0;
+      for(let i=0;i<array.length;i++){
+          suma=suma+parseInt(array[i].rating);
+          cont++;
+      }
+      return suma/cont;
+  }
+  
+  let prom=0;
+  if(quantity) prom=promedio(array);
+  console.log(prom)
+return (
+  <div className='prom'>
+    <Box component="fieldset" mb={3} borderColor="transparent">        
+      <Rating name="read-only" value={prom} precision={0.5} readOnly />
+      <Typography component="legend">Votes {quantity} </Typography>
+      <br/>
+      <br/>
+
+       <Button variant="contained" color="primary" onClick={openPopover}>
+          Comments
+      </Button>
+      <Popover 
+      open={Boolean(anchor)}
+      anchorEl={anchor}
+      anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right"
+      }}
+      transformOrigin={{
+          vertical: "top",
+          horizontal: "right"
+      }}
+      onClose={() =>setAnchor(null)}>
+      <Typography variant="legend"> {render}    </Typography> 
+
+      </Popover>
+    </Box>
+  </div>
+);
+
+}
