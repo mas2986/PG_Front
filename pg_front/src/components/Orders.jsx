82a0a596cb006aa6ec47582b125b15c80c0b3605
Marcos Saac <ing.msaac@gmail.com>
2022-09-18 03:49:34 -0300

    agregado de funcion para modificar stock de producto comprado y agregado de campo fecha

--- pg_front/src/components/Orders.jsx
+++ pg_front/src/components/Orders.jsx
@@ -16,8 +16,8 @@ import {
   Typography,
   Tooltip,
 } from '@mui/material';
-import { Delete, Password, AccountCircle } from '@mui/icons-material';
-import { getAllOrders, changeOrderStatus } from '../redux/action';
+
+import { getAllOrders, changeOrderStatus,editProduct as editStock } from '../redux/action';
 import { useSelector, useDispatch } from 'react-redux';
 
 const Orders = () => {
@@ -26,21 +26,49 @@ const Orders = () => {
   const [rowSelection, setRowSelection] = useState({});
   const dispatch = useDispatch();
   const orders = useSelector(state => state.order);
+  const products = useSelector(state=>state.productAdmin)
   const status = ['cancelled', 'completed']
+  console.log(orders);
+  //console.log(products)
+  //  console.log(idProducts)
+  const handleStatus = (row,value) => {        
+    row.original.orderStatus = value;
+    let {id,orderStatus,email,total,idProduct,titleProduct} = row.original;   
+    Swal.fire({
+      title: `Do you want change at ${row.original.orderStatus} to Order number ${row.original.id}?`,
+      text: "You can be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, change status!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        dispatch(changeOrderStatus(id,orderStatus,email,total))
+        setEdit(true);  
+        if(value==='completed'){
+          idProduct = idProduct.split(', ')
+          titleProduct = titleProduct.split(', ')
+          var productPurchase = []
+          var qtyProduct = []  
+          for(let i = 0; i<idProduct.length;i++){
+            productPurchase[i] = products.find((e)=>e.id===parseInt(idProduct[i]))
+            qtyProduct[i] = titleProduct.filter(e=>e===productPurchase[i].title).length
+            productPurchase[i].stock = productPurchase[i].stock - qtyProduct[i];
+            const { id,stock } = productPurchase[i];                  
+            dispatch(editStock(id,stock));
+          }         
+        }              
+      }
+    })
+  }
 
   useEffect(() => {
     if (orders.length === 0 || edit) {
-      dispatch(getAllOrders());
+      dispatch(getAllOrders());  
+      setEdit(() => false)
     }
-    setEdit(() => false)
   }, [edit])
-
-  const handleStatus = (row,value) => {        
-    row.original.orderStatus = value;
-    const {id,orderStatus,email} = row.original;
-    setEdit(true);
-    dispatch(changeOrderStatus(id,orderStatus,email))
-  }
 
   const columns = useMemo(
     () => [
@@ -50,7 +78,20 @@ const Orders = () => {
         enableColumnOrdering: false,
         enableFiltering: false,
         enableEditing: false, //disable editing on this column
-        size: 20,
+        size: 40,
+      },
+      {
+        accessorKey: 'createdAt',
+        header: 'Creation Date',
+        enableEditing: false,
+        Cell:({cell}) =>{
+          let createdAt = new Date(cell.getValue())
+          return(
+            <>
+            {`${createdAt.getMonth()+1}-${createdAt.getDate()}-${createdAt.getFullYear()}`}
+            </>
+          )
+        }
       },
       {
         accessorKey: 'email',
@@ -58,8 +99,14 @@ const Orders = () => {
         enableEditing: false,
       },
       {
-        accessorKey: 'adressShipping',
-        header: 'Address Shipping',
+        accessorKey: 'idProduct',
+        header: 'Purchase products',
+        enableEditing: false,
+      },
+
+      {
+        accessorKey: 'titleProduct',
+        header: 'Product Purchase',
         enableEditing: false,
       },
       {
@@ -89,7 +136,8 @@ const Orders = () => {
           </>
         ),
         muiTableBodyCellEditTextFieldProps: ({cell,row})=>({
-          select: true, //change to select for a dropdown
+          //select: cell.getValue()!=="created"?false:true, //change to select for a dropdown
+          select: true,
           children:
             status.map((e) => (
               <MenuItem key={e} 
@@ -118,14 +166,15 @@ const Orders = () => {
       }}
       columns={columns}
       data={orders}
-      //initialState={{ columnVisibility: { id: false } }}
+      enableColumResizing
+      initialState={{ columnVisibility: { idProduct: false, titleProduct:false } }}
       positionRowActions="right"
-      /* muiTableHeadCellProps={{
+      muiTableHeadCellProps={{
         sx: {
-          backgroundColor: 'black',
+          backgroundColor: '#0833A2',
           color: 'white',
         },
-      }} */
+      }} 
       /*    muiTableBodyCellProps={{
            sx:{
              backgroundColor:'#9c9c9c'
@@ -143,7 +192,11 @@ const Orders = () => {
       muiTableBodyCellEditTextFieldProps={({ row }) => ({
         //onBlur is more efficient, but could use onChange instead
          onBlur: (event) => {
-           handleStatus(row, event.target.value);
+           row.original.orderStatus==="created"
+           ?
+           handleStatus(row, event.target.value)
+           :
+           null;
          },
       })}
       muiSelectCheckboxProps={({ row }) => ({
