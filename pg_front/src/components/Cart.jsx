f6275032eb070d5b4d60b5d891b7e5c6f0ac8de2
Federico Iglesia <federico.iglesia@outlook.com>
2022-09-19 20:25:06 -0300

    adding outside click to close cart

--- pg_front/src/components/Cart.jsx
+++ pg_front/src/components/Cart.jsx
@@ -10,6 +10,7 @@ import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import FormPropsTextFields from "./Formulario";
 import { Link, useHistory } from "react-router-dom";
+import OutsideClickHandler from "react-outside-click-handler";
 import {
   deleteFromCart,
   deleteAllFromCart,
@@ -107,24 +108,45 @@ function Cart() {
           />
         </StyledBadge>
       </div>
-      {cartDisplay && (
-        <Box
-          className={n["cart-container"]}
-          // onMouseEnter={keepIn}
-          // onMouseLeave={toggle}
-          sx={{ position: "absolute", top: "1rem", right: "2rem" }}
-        >
+      <OutsideClickHandler
+        onOutsideClick={() => {
+          setCartDisplay(false);
+        }}
+      >
+        {cartDisplay && (
           <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-            }}
+            className={n["cart-container"]}
+            // onMouseEnter={keepIn}
+            // onMouseLeave={toggle}
+            sx={{ position: "absolute", top: "1rem", right: "2rem" }}
           >
-            {items.length ? (
-              <Box
-                display="flex"
-                sx={{ alignItems: "center", minWidth: "20rem" }}
-              >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              {items.length ? (
+                <Box
+                  display="flex"
+                  sx={{ alignItems: "center", minWidth: "20rem" }}
+                >
+                  <Typography
+                    align="left"
+                    variant="h5"
+                    color="primary"
+                    flexGrow={1}
+                    // sx={{ marginBottom: "0.5rem" }}
+                  >
+                    My Cart
+                  </Typography>
+                  <DeleteForeverIcon
+                    sx={{ color: "#a52a2a", fontSize: "3rem" }}
+                    className={n["cart-delete-all-icon"]}
+                    onClick={deleteAll}
+                  />
+                </Box>
+              ) : (
                 <Typography
                   align="left"
                   variant="h5"
@@ -134,154 +156,142 @@ function Cart() {
                 >
                   My Cart
                 </Typography>
-                <DeleteForeverIcon
-                  sx={{ color: "#a52a2a", fontSize: "3rem" }}
-                  className={n["cart-delete-all-icon"]}
-                  onClick={deleteAll}
-                />
-              </Box>
-            ) : (
-              <Typography
-                align="left"
-                variant="h5"
-                color="primary"
-                flexGrow={1}
-                // sx={{ marginBottom: "0.5rem" }}
-              >
-                My Cart
-              </Typography>
-            )}
-            {items.length ? (
-              <Box>
-                <Box
-                  sx={{
-                    maxHeight: "22rem",
-                    overflowY: "scroll",
-                    overflowX: "hidden",
-                    marginBottom: "0.5rem",
-                  }}
-                >
-                  {items.map((i, idx) => {
-                    return (
-                      <Box
-                        key={idx}
-                        sx={{
-                          padding: "1rem",
-                          width: "20rem",
-                          height: "16.5rem",
-                          margin: "1rem 0",
-                          border: "1px solid black",
-                          borderRadius: "3px",
-                          background: "#fff",
-                        }}
-                      >
-                        <Box display="flex">
-                          <Typography
-                            variant="h7"
-                            color="primary"
-                            flexGrow={1}
-                            sx={{ fontWeight: 700, marginBottom: "0.5rem" }}
-                          >
-                            {i.title[0].toUpperCase() + i.title.substring(1)}
-                          </Typography>
-                          <DeleteOutlineIcon
-                            sx={{ color: "#a52a2a" }}
-                            className={n["cart-delete-icon"]}
-                            onClick={() => deleteItem(idx)}
-                          />
-                        </Box>
-                        <Box sx={{ display: "flex", flexDirection: "row" }}>
-                          <Box flexGrow={1}>
-                            <Link to={`/detail/${i.id}`}>
-                              <img
-                                src={i.image}
-                                width="100px"
-                                height="80px"
-                                style={{ marginBottom: "0.5rem" }}
-                              />
-                            </Link>
-                            <Typography sx={{ textDecoration: "underline" }}>
-                              {i.brand}
-                            </Typography>
-                            <Typography sx={{ fontStyle: "italic" }}>
-                              {i.description}
-                            </Typography>
-                            <select
-                              value={i.qty}
-                              //declaring the handler fn here as i need to use the map method current item (i)
-                              onChange={(e) => {
-                                i.qty = e.target.value;
-                                dispatch(sendItemNum(e.target.value));
-                                //changing state to force re render of price
-                                setQty((prev) => prev + 1);
-                                //saving item with updated qty
-                                localStorage.setItem(
-                                  "items",
-                                  JSON.stringify(items)
-                                );
-                              }}
-                            >
-                              <option value={1}>1</option>
-                              <option value={2}>2</option>
-                              <option value={3}>3</option>
-                              <option value={4}>4</option>
-                              <option value={5}>5</option>
-                            </select>
-                          </Box>
-                          <Typography
-                            sx={{ margin: "5rem 3rem 0 0", fontSize: "1.2rem" }}
-                          >
-                            ${i.price * i.qty || detail?.price}.00
-                          </Typography>
-                        </Box>
-                      </Box>
-                    );
-                  })}
-                </Box>
-                <Box display="flex">
-                  <Typography variant="h6" color="primary" flexGrow={1}>
-                    Total
-                  </Typography>
-                  <Typography variant="h6" color="primary">
-                    $
-                    {items.reduce(
-                      (prev, curr) => prev + curr.price * curr.qty,
-                      0
-                    ) ||
-                      items.reduce(
-                        (prev, curr) => prev + detail?.price * curr.qty,
-                        0
-                      )}
-                    .00
-                  </Typography>
-                </Box>
-                <Box display="flex" sx={{ justifyContent: "center" }}>
-                  <Button
-                    onClick={(e) => handlePay(e)}
+              )}
+              {items.length ? (
+                <Box>
+                  <Box
                     sx={{
-                      margin: "0.5rem",
-                      border: "1px solid #000",
+                      maxHeight: "22rem",
+                      overflowY: "scroll",
+                      overflowX: "hidden",
+                      marginBottom: "0.5rem",
                     }}
                   >
-                    Proceed to checkout
-                  </Button>
+                    {items.map((i, idx) => {
+                      return (
+                        <Box
+                          key={idx}
+                          sx={{
+                            padding: "1rem",
+                            width: "20rem",
+                            height: "16.5rem",
+                            margin: "1rem 0",
+                            border: "1px solid black",
+                            borderRadius: "3px",
+                            background: "#fff",
+                          }}
+                        >
+                          <Box display="flex">
+                            <Typography
+                              variant="h7"
+                              color="primary"
+                              flexGrow={1}
+                              sx={{ fontWeight: 700, marginBottom: "0.5rem" }}
+                            >
+                              {i.title[0].toUpperCase() + i.title.substring(1)}
+                            </Typography>
+                            <DeleteOutlineIcon
+                              sx={{ color: "#a52a2a" }}
+                              className={n["cart-delete-icon"]}
+                              onClick={() => deleteItem(idx)}
+                            />
+                          </Box>
+                          <Box sx={{ display: "flex", flexDirection: "row" }}>
+                            <Box flexGrow={1}>
+                              <Link to={`/detail/${i.id}`}>
+                                <img
+                                  src={i.image}
+                                  width="100px"
+                                  height="80px"
+                                  style={{ marginBottom: "0.5rem" }}
+                                />
+                              </Link>
+                              <Typography sx={{ textDecoration: "underline" }}>
+                                {i.brand}
+                              </Typography>
+                              <Typography sx={{ fontStyle: "italic" }}>
+                                {i.description}
+                              </Typography>
+                              <select
+                                value={i.qty}
+                                //declaring the handler fn here as i need to use the map method current item (i)
+                                onChange={(e) => {
+                                  i.qty = e.target.value;
+                                  dispatch(sendItemNum(e.target.value));
+                                  //changing state to force re render of price
+                                  setQty((prev) => prev + 1);
+                                  //saving item with updated qty
+                                  localStorage.setItem(
+                                    "items",
+                                    JSON.stringify(items)
+                                  );
+                                }}
+                              >
+                                <option value={1}>1</option>
+                                <option value={2}>2</option>
+                                <option value={3}>3</option>
+                                <option value={4}>4</option>
+                                <option value={5}>5</option>
+                              </select>
+                            </Box>
+                            <Typography
+                              sx={{
+                                margin: "5rem 3rem 0 0",
+                                fontSize: "1.2rem",
+                              }}
+                            >
+                              ${i.price * i.qty || detail?.price}.00
+                            </Typography>
+                          </Box>
+                        </Box>
+                      );
+                    })}
+                  </Box>
+                  <Box display="flex">
+                    <Typography variant="h6" color="primary" flexGrow={1}>
+                      Total
+                    </Typography>
+                    <Typography variant="h6" color="primary">
+                      $
+                      {items.reduce(
+                        (prev, curr) => prev + curr.price * curr.qty,
+                        0
+                      ) ||
+                        items.reduce(
+                          (prev, curr) => prev + detail?.price * curr.qty,
+                          0
+                        )}
+                      .00
+                    </Typography>
+                  </Box>
+                  <Box display="flex" sx={{ justifyContent: "center" }}>
+                    <Button
+                      onClick={(e) => handlePay(e)}
+                      sx={{
+                        margin: "0.5rem",
+                        border: "1px solid #000",
+                      }}
+                    >
+                      Proceed to checkout
+                    </Button>
+                  </Box>
                 </Box>
-              </Box>
-            ) : (
-              <Typography
-                sx={{
-                  whiteSpace: "nowrap",
-                  width: "12rem",
-                  marginRight: "5rem",
-                  marginTop: "1rem",
-                }}
-              >
-                There are no items in the cart
-              </Typography>
-            )}
+              ) : (
+                <Typography
+                  sx={{
+                    whiteSpace: "nowrap",
+                    width: "12rem",
+                    marginRight: "5rem",
+                    marginTop: "1rem",
+                  }}
+                >
+                  There are no items in the cart
+                </Typography>
+              )}
+            </Box>
           </Box>
-        </Box>
-      )}
+        )}
+      </OutsideClickHandler>
     </>
   );
 }
