0fccc80cb7c42d9668619f77d0dd58a13de59929
Federico Iglesia <federico.iglesia@outlook.com>
2022-09-21 12:29:26 -0300

    img clickeable to detail / deleted blank lines in detail

--- pg_front/src/components/Detail.jsx
+++ pg_front/src/components/Detail.jsx
@@ -1,7 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, Redirect, useParams } from "react-router-dom";
-import { detailProduct, getAllOrders, createOrder, removeDupsCart, addToCartDetail, mercadoPago, getReviews, getAllUsers, getOrderByUser } from "../redux/action";
+import {
+  detailProduct,
+  getAllOrders,
+  createOrder,
+  removeDupsCart,
+  addToCartDetail,
+  mercadoPago,
+  getReviews,
+  getAllUsers,
+  getOrderByUser,
+} from "../redux/action";
 import d from "./Detail.module.css";
 import Nav2 from "./Nav2.jsx";
 import Button from "@mui/material/Button";
@@ -9,7 +19,7 @@ import { CircularProgress } from "@mui/material";
 import plop from "../asset/plop.mp3";
 import Section from "./Section";
 import { useHistory } from "react-router-dom";
-import RatingProm from "./RatingProm"
+import RatingProm from "./RatingProm";
 import Review from "./Review";
 
 export default function Detail() {
@@ -18,65 +28,53 @@ export default function Detail() {
   const [qty, setQty] = useState(1);
   const history = useHistory();
   const [render, setRender] = useState(false);
-  const order = useSelector((state) => state.order)
-  const user = useSelector((state) => state.user)
-  const UserId = user.id
-  const cartItem = useSelector(state=>state.cartItems)
-  const userOrder = order.filter((item) => item.userId === UserId) && (order.filter(item => item.orderStatus === "completed")) 
- 
-  console.log(userOrder)
+  const order = useSelector((state) => state.order);
+  const user = useSelector((state) => state.user);
+  const UserId = user.id;
+  const cartItem = useSelector((state) => state.cartItems);
+  const userOrder =
+    order.filter((item) => item.userId === UserId) &&
+    order.filter((item) => item.orderStatus === "completed");
 
-  
-  const users =  useSelector((state) => state.users);
-  
-  
+  console.log(userOrder);
+
+  const users = useSelector((state) => state.users);
 
   let official = 149;
-  
-  const review = useSelector((state) => state.reviews);
- 
 
- 
-  
-  let quantity=0;
+  const review = useSelector((state) => state.reviews);
+
+  let quantity = 0;
   let array = [];
-  let comments = ""
-  let uservacio = ""
-  let rendercomment =[]
-  let ramdom = []
-  
-  
+  let comments = "";
+  let uservacio = "";
+  let rendercomment = [];
+  let ramdom = [];
+
   for (let i = 0; i < review.length; i++) {
-      if(review[i].productId === Number(id)) {
-          array.push(review[i])
-          quantity += 1;
-          comments = (review[i].comment + " ")
-           users.forEach(element => { if(element.id === review[i].userId) uservacio = element.id       
-          });
-          rendercomment[i] = {uservacio: uservacio,
-                      comments:comments } 
-        
-        
-          
-         
-      } else{
-          array
-      }
-      
+    if (review[i].productId === Number(id)) {
+      array.push(review[i]);
+      quantity += 1;
+      comments = review[i].comment + " ";
+      users.forEach((element) => {
+        if (element.id === review[i].userId) uservacio = element.id;
+      });
+      rendercomment[i] = { uservacio: uservacio, comments: comments };
+    } else {
+      array;
+    }
   }
 
-  const prueba = rendercomment.filter(el => el)
+  const prueba = rendercomment.filter((el) => el);
 
   useEffect(() => {
     dispatch(detailProduct(id));
-    dispatch(getReviews())
-    dispatch(getOrderByUser(UserId))
-    dispatch(getAllUsers())
+    dispatch(getReviews());
+    dispatch(getOrderByUser(UserId));
+    dispatch(getAllUsers());
   }, [dispatch, id]);
 
   const detail = useSelector((state) => state.detail);
-
-
 
   function addCart() {
     new Audio(plop).play();
@@ -89,20 +87,19 @@ export default function Detail() {
   async function handlePay(e) {
     e.preventDefault();
     if (cartItem.filter((c) => c.id !== detail.id)) {
-      addCart()}
+      addCart();
+    }
     dispatch(mercadoPago({ price: detail.price * official }));
-    history.push('/entrega')
+    history.push("/entrega");
   }
 
-
-
-  console.log(detail ?.stock)
+  console.log(detail?.stock);
 
   return (
     <center>
       <Nav2 />
 
-      <Section/>
+      <Section />
       <div className={d.detailPage}>
         {/*console.log(detail)*/}
         {detail ? (
@@ -112,11 +109,10 @@ export default function Detail() {
                 src={detail.img ? detail.img : detail.image}
                 alt="Image not found"
                 className={d.bigImage}
-                />
+              />
             </div>
 
             <div className={d.productDetail}>
-
               <h1 className={d.title}>
                 {detail.title && detail.title.toUpperCase()}
               </h1>
@@ -126,57 +122,59 @@ export default function Detail() {
               <p className={d.price}>
                 Price: ${detail.price && detail.price},00
               </p>
-              <RatingProm id={id} reviews={review} users={users}/>
+              <RatingProm id={id} reviews={review} users={users} />
 
-            <Button onClick={(e)=> handlePay(e)} variant="contained" size="small" sx={{
-              padding:2
-            }} className={d.buyButton}>
-              BUY
-            </Button>
-            <Button variant="outlined" size="small" onClick={addCart} className={d.cartButton}>
-              ADD TO CART
-            </Button>
-
-            <Link to="/products">
               <Button
-                //   href={`http://localhost:3000/home`} cambio a routing por link para que no se pierda el carrito
-                // variant="contained"
-                // color="primary"
-                size="large"
-                className={d.homeButton}
-                >
-                GO BACK
+                onClick={(e) => handlePay(e)}
+                variant="contained"
+                size="small"
+                sx={{
+                  padding: 2,
+                }}
+                className={d.buyButton}
+              >
+                BUY
               </Button>
-            </Link>
-            <div className = {d.detailSection}>
-              <p className={d.description}>
-                {detail.description && detail.description}
-              </p>
-            
-              <p className={d.brand}>
-                Brand: {detail.brand && detail.brand}
-              </p>
-              
-              <p className={d.sport}>
-                Sport: {detail.sport && detail.sport}
-              </p>
+              <Button
+                variant="outlined"
+                size="small"
+                onClick={addCart}
+                className={d.cartButton}
+              >
+                ADD TO CART
+              </Button>
+
+              <Link to="/products">
+                <Button
+                  //   href={`http://localhost:3000/home`} cambio a routing por link para que no se pierda el carrito
+                  // variant="contained"
+                  // color="primary"
+                  size="large"
+                  className={d.homeButton}
+                >
+                  GO BACK
+                </Button>
+              </Link>
+              <div className={d.detailSection}>
+                <p className={d.description}>
+                  {detail.description && detail.description}
+                </p>
+
+                <p className={d.brand}>Brand: {detail.brand && detail.brand}</p>
+
+                <p className={d.sport}>Sport: {detail.sport && detail.sport}</p>
+              </div>
+              <div>{<Review id={id} />}</div>
             </div>
-            <div>
-                  { <Review id={id}/> }
-             </div>     
-
-            </div>
-
-
-
           </div>
-
         ) : (
-            <CircularProgress color="success" sx={{
-              marginTop: 35
-            }} />
-          )}
-
+          <CircularProgress
+            color="success"
+            sx={{
+              marginTop: 35,
+            }}
+          />
+        )}
       </div>
     </center>
   );
